1749ed2cc215d88ce03557822ce1ed4ed3f06b9a
Greimore1 <a4osbourne@outlook.com>
2024-12-16 06:23:55 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,24 @@ provider "aws" {
   region = var.aws_region
 }
 
+# ECS Cluster
 resource "aws_ecs_cluster" "slack_chatbot_cluster" {
   name = "slack-chatbot-cluster"
 }
 
+# ECS Task Definition with Execution Role
 resource "aws_ecs_task_definition" "slack_chatbot_task" {
   family                   = "slack-chatbot-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
   memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
   container_definitions = jsonencode([{
     name  = "slack-chatbot"
     image = var.container_image
-    
+
     portMappings = [{
       containerPort = 8080
       hostPort      = 8080
@@ -33,12 +37,14 @@ resource "aws_ecs_task_definition" "slack_chatbot_task" {
   }])
 }
 
+# ECS Service
 resource "aws_ecs_service" "slack_chatbot_service" {
   name            = "slack-chatbot-service"
   cluster         = aws_ecs_cluster.slack_chatbot_cluster.id
   task_definition = aws_ecs_task_definition.slack_chatbot_task.arn
   launch_type     = "FARGATE"
   desired_count   = 1
+
   network_configuration {
     subnets          = var.subnet_ids
     security_groups  = var.security_group_ids
@@ -46,6 +52,7 @@ resource "aws_ecs_service" "slack_chatbot_service" {
   }
 }
 
+# IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "ecs-task-execution-role"
   assume_role_policy = jsonencode({
@@ -62,6 +69,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
+# Attach the Execution Policy to the Role
 resource "aws_iam_policy_attachment" "ecs_task_execution_policy" {
   name       = "ecs-task-execution-policy-attachment"
   roles      = [aws_iam_role.ecs_task_execution_role.name]
