92a9a4cc9e259109adca33f4184554b044d2e03e
Greimore1 <a4osbourne@outlook.com>
2024-12-16 04:42:36 +0000

    Update variables.tf
    added container image var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,14 @@
+variable "container_image" {
+  description = "Docker container image"
+  type        = string
+}
+
 variable "subnet_ids" {
-  description = "Subnets for ECS service"
+  description = "List of subnet IDs"
   type        = list(string)
 }
 
 variable "security_group_ids" {
-  description = "Security groups for ECS service"
+  description = "List of security group IDs"
   type        = list(string)
 }
