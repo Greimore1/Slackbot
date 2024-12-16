62a642c67b6da78621c46e0e7fa659f381097d4a
Greimore1 <a4osbourne@outlook.com>
2024-12-16 04:57:45 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-west-2"
-}
-
-variable "container_image" {
-  description = "ECR container image"
-  type        = string
-}
-
 resource "aws_ecs_cluster" "slack_chatbot_cluster" {
   name = "slack-chatbot-cluster"
 }
@@ -23,7 +12,6 @@ resource "aws_ecs_task_definition" "slack_chatbot_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
   memory                   = 512
-
   container_definitions = jsonencode([{
     name  = "slack-chatbot"
     image = var.container_image
@@ -32,7 +20,6 @@ resource "aws_ecs_task_definition" "slack_chatbot_task" {
       containerPort = 8080
       hostPort      = 8080
     }]
-
     environment = [
       {
         name  = "SLACK_BOT_TOKEN"
@@ -51,9 +38,7 @@ resource "aws_ecs_service" "slack_chatbot_service" {
   cluster         = aws_ecs_cluster.slack_chatbot_cluster.id
   task_definition = aws_ecs_task_definition.slack_chatbot_task.arn
   launch_type     = "FARGATE"
-
-  desired_count = 1
-
+  desired_count   = 1
   network_configuration {
     subnets          = var.subnet_ids
     security_groups  = var.security_group_ids
