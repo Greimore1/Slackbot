c5a6f1a7cbe15f3da10bca8af9204724625281ad
Greimore1 <a4osbourne@outlook.com>
2024-12-16 04:35:06 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
-variable "TF_VAR_SUBNET_IDS" {
+variable "subnet_ids" {
   description = "Subnets for ECS service"
   type        = list(string)
 }
 
-variable "TF_VAR_SECURITY_GROUP_IDS" {
+variable "security_group_ids" {
   description = "Security groups for ECS service"
   type        = list(string)
 }
