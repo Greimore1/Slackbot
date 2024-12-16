2727808076907e10ae6dc70a3145e1c49f6ffc0c
Greimore1 <a4osbourne@outlook.com>
2024-12-16 04:56:07 +0000

    Update variables.tf
    added aws region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "container_image" {
-  description = "Docker container image"
+  description = "ECR container image"
   type        = string
 }
 
@@ -11,4 +11,10 @@ variable "subnet_ids" {
 variable "security_group_ids" {
   description = "List of security group IDs"
   type        = list(string)
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
 }
