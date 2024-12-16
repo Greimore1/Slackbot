8b20141fee51c44bae247acad5717c3554643d79
Greimore1 <a4osbourne@outlook.com>
2024-12-16 05:04:29 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,31 +2,16 @@ variable "container_image" {
   description = "ECR container image"
   type        = string
 }
-
 variable "subnet_ids" {
   description = "List of subnet IDs"
   type        = list(string)
 }
-
 variable "security_group_ids" {
   description = "List of security group IDs"
   type        = list(string)
 }
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "eu-west-2"
-}
-
-variable "slack_bot_token" {
-  description = "Slack Bot Token"
-  type        = string
-  sensitive   = true
-}
-
-variable "gemini_api_key" {
-  description = "Gemini API Key"
-  type        = string
-  sensitive   = true
 }
